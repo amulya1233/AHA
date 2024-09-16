c847054e737c7fcbb2b642b67cd6e42e2d343fe3
amulya <amulyavmurthy7@gmail.com>
2024-09-17 00:07:39 +0530

    first commit

---
+++ akka.py
@@ -0,0 +1,4 @@
+message = input("What message would you like to print? ")
+n = int(input("How many times would you like to print it? "))
+for _ in range(n):
+    print(message)
---
+++ bhava.py
@@ -0,0 +1,3 @@
+n = int(input("How many times would you like to print it? "))
+for _ in range(n):
+    print("i am sorry")
